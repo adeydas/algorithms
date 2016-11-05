332ff147e09833a2fd0a9478d0b34f520ed697f0
Abhishek Dey Das <abhishek@abhis.ws>
2016-11-05 13:05:40 -0700

    Choose around the center #86

---
+++ src/main/java/ws/abhis/education/leetcode/LongestPalindromeSubstr.java
@@ -0,0 +1,30 @@
+package ws.abhis.education.leetcode;
+
+/**
+ * https://code.abhis.ws/Tech_Interviews/Algorithms_Maven/issues/86
+ */
+public class LongestPalindromeSubstr {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                // choose the start and end points around the midpoint.
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
---
+++ src/test/java/ws/abhis/education/leetcode/LongestPalindromeSubstrTest.java
@@ -0,0 +1,17 @@
+package ws.abhis.education.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by abhishek on 11/5/16.
+ */
+public class LongestPalindromeSubstrTest {
+    @Test
+    public void longestPalindrome() throws Exception {
+        LongestPalindromeSubstr longestPalindromeSubstr = new LongestPalindromeSubstr();
+        longestPalindromeSubstr.longestPalindrome("abba");
+    }
+
+}
